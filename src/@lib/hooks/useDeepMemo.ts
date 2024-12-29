319dd53e5fb396e283c4bca642d6531e9693d0c4
JunmanChoi <junman9595@gmail.com>
2024-12-30 01:29:19 +0900

    feat: useDeepMemo 커스텀 훅 구현 - equals함수 단순 교체를 통해 구현

--- src/@lib/hooks/useDeepMemo.ts
+++ src/@lib/hooks/useDeepMemo.ts
@@ -4,6 +4,7 @@ import { useMemo } from './useMemo';
 import { deepEquals } from '../equalities';
 
 export function useDeepMemo<T>(factory: () => T, deps: DependencyList): T {
-  // 직접 작성한 useMemo를 참고해서 만들어보세요.
-  return useMemo(factory, deps, deepEquals);
+  const memoized = useMemo(factory, deps, deepEquals);
+
+  return memoized;
 }
