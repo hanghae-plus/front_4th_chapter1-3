faf1325755d3e66122aa3e8e52c60e99fb815001
Geunbaek <rmsqor103@naver.com>
2024-12-29 19:44:52 +0900

    feat: shallowEquals 추가

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,33 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  // 원시타입의 값이 동일하거나 기존 객체의 주소값이 바뀌지 않은 경우
+  if (objA === objB) {
+    return true;
+  }
+
+  // null인 경우
+  if (objA === null || objB === null) {
+    return objA === objB;
+  }
+
+  // 나머지 원시값들 처리
+  if (typeof objA !== "object" || typeof objB !== "object") {
+    return objA === objB;
+  }
+
+  const objAKeys = Object.keys(objA);
+  const objBKeys = Object.keys(objB);
+
+  if (objAKeys.length !== objBKeys.length) {
+    return false;
+  }
+
+  for (const key of objAKeys) {
+    const valueA = Reflect.get(objA, key);
+    const valueB = Reflect.get(objB, key);
+    if (valueA !== valueB) {
+      return false;
+    }
+  }
+
+  return true;
 }
