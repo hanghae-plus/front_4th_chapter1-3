00629a17b2ac2b4aee39cbeeab2c90fe6f5f6f54
nogy21 <nogy21@gmail.com>
2024-12-30 00:15:57 +0900

    feat: 컴포넌트를 메모이제이션하는 memo 함수 구현
    - 이전 props와 현재 props를 비교하여 다를 경우에만 새롭게 렌더링
    - 정적 프로퍼티 복사 로직 미구현 ('propTypes', 'contextTypes', 'defaultProps', 'displayName')

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ComponentType, createElement } from "react";
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { useRef } from "../hooks";
 
+/**
+ * 컴포넌트를 메모이제이션하는 함수
+ * @param Component
+ * @param _equals
+ * @returns
+ */
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  const MemoizedComponent = (props: P) => {
+    const prevPropsRef = useRef<P | null>(null);
+    const renderRef = useRef<React.ReactElement | null>(null);
+
+    // 현재 props와 이전 props를 비교하여 변경 여부를 확인
+    if (!prevPropsRef.current || !_equals(prevPropsRef.current, props)) {
+      prevPropsRef.current = props;
+      renderRef.current = createElement(Component, props); // 새로 렌더링
+    }
+
+    return renderRef.current;
+  };
+
+  return MemoizedComponent;
 }
