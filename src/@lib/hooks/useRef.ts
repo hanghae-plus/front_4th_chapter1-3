5bb5bac4bde3c3f8e7f35a196862b09841fdcc8b
jch1223 <ico1828@gmail.com>
2024-12-30 00:23:00 +0900

    useRef 인자로 undefined 허용

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,6 +1,6 @@
 import React from "react";
 
-export function useRef<T>(initialValue: T): { current: T } {
-  const [ref] = React.useState({ current: initialValue });
+export function useRef<T>(initialValue?: T): { current?: T } {
+  const [ref] = React.useState<{ current?: T }>({ current: initialValue });
   return ref;
 }
