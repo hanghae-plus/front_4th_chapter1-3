e4921cd8eb8f43f7c2a0c7acc3e444e6d650ce12
jungeun0712 <kje970712@naver.com>
2024-12-29 21:34:41 +0900

    feat: useMemo 구현

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,13 +1,8 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export function useRef<T>(initialValue: T): { current: T } {
-  // React의 useState를 이용해서 만들어보세요.
-  const [ref] = useState<{ current: T }>({ current: initialValue });
-
-  // ref current 값이 바뀌면 initialValue를 업데이트
-  useEffect(() => {
-    ref.current = initialValue;
-  }, [initialValue, ref]);
+  // { current: initialValue }을 초기값으로 가지는 ref 생성
+  const [ref] = useState<{ current: T }>(() => ({ current: initialValue }));
 
   return ref;
 }
