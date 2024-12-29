06d60ab03ccad45cc8bb2548270c07b02a3e77aa
Dayglow0926 <dayglow0926@gmail.com>
2024-12-29 20:53:16 +0900

    feat: deepEquals 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,22 @@
+//    - 재귀적으로 각 속성에 대해 deepEquals 호출
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+  // 2. 둘 다 객체인 경우:
+  //    - 배열인지 확인
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return objA.every((v, i) => deepEquals(v, objB[i]));
+  }
+
+  //    - 객체의 키 개수가 다른 경우 처리
+  if (
+    typeof objA === "object" &&
+    typeof objB === "object" &&
+    Object.keys(objA).length === Object.keys(objB).length
+  ) {
+    return Object.keys(objA).every((key) => deepEquals(objA[key], objB[key]));
+  }
+
+  return false;
 }
