a4d0c1cd44c90861cd27ddd6ddd81051c942c3af
jungeun0712 <kje970712@naver.com>
2024-12-29 18:02:55 +0900

    feat: useRef 추가

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,4 +1,13 @@
+import { useEffect, useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
   // React의 useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  const [ref] = useState<{ current: T }>({ current: initialValue });
+
+  // ref current 값이 바뀌면 initialValue를 업데이트
+  useEffect(() => {
+    ref.current = initialValue;
+  }, [initialValue, ref]);
+
+  return ref;
 }
