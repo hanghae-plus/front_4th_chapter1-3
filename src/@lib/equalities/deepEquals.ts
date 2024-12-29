dfbfb6df2f96bcfff153b28f3cc547e0899a9925
Changyeop Song <waitwait0301@naver.com>
2024-12-29 18:04:47 +0900

    feat: deepEqual 함수 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,30 @@
+// deepEquals 함수는 두 값의 깊은 비교를 수행합니다.
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (Object.is(objA, objB)) return true;
+
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  const objAKeys = Object.keys(objA || {});
+  const objBKeys = Object.keys(objB || {});
+
+  if (objAKeys.length !== objBKeys.length) {
+    return false;
+  }
+
+  for (const key of objAKeys) {
+    // 재귀적으로 각 속성에 대해 deepEquals 호출
+    if (!deepEquals(objA[key as keyof T], objB[key as keyof T])) {
+      return false;
+    }
+  }
+
+  return true;
 }
