15738a93a785dccd0c6edc416d539ec47cdd99e2
JunmanChoi <junman9595@gmail.com>
2024-12-30 02:44:26 +0900

    feat: hoc memo 컴포넌트 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,24 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from '../equalities';
-import { ComponentType } from 'react';
+import { ComponentType, createElement } from 'react';
+import { useMemo, useRef } from '../hooks';
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals
 ) {
-  return Component;
+  // React.memo를 직접 구현해보세요.
+  const MemoComponent: ComponentType<P> = (props: P) => {
+    // 얘가 실제 Props 변경을 감지하는 역할을 합니다.
+    const propsRef = useRef<P>(props);
+
+    if (!_equals(propsRef.current, props)) {
+      propsRef.current = props;
+    }
+    return useMemo(
+      () => createElement(Component, propsRef.current),
+      [propsRef.current]
+    );
+  };
+  return MemoComponent;
 }
