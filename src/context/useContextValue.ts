d05b2c50883e1afe9ed3f7fb3452fd500e4c7313
nogy21 <nogy21@gmail.com>
2024-12-30 03:29:48 +0900

    refactor: useContextValue 에러 메시지
    - Context 이름을 활용한 명확한 에러 메시지 추가

--- src/context/useContextValue.ts
+++ src/context/useContextValue.ts
@@ -2,15 +2,21 @@ import { useContext } from "react";
 
 /**
  * context를 안전하게 사용하기 위한 context 커스텀 훅
- * @description context를 안전하게 사용하는 함수
+ * @description React의 useContext를 래핑한 커스텀 훅
+ * - Context가 Provider 내부에서 사용되는지 확인하고 Provider 외부에서 사용될 경우 구체적인 에러 메시지와 함께 에러를 발생
  * @param context
  * @returns context 값
  * @throws Error context가 사용되는 컴포넌트을 찾을 수 없는 경우 에러 발생
  */
 export const useContextValue = <T>(context: React.Context<T>) => {
   const contextValue = useContext(context);
-  if (!contextValue) {
-    throw new Error(`${context} must be used within an AppProvider`);
+
+  if (contextValue == null) {
+    const contextName = context.displayName || "Unknown Context";
+    throw new Error(
+      `${contextName} must be used within an ${contextName}Provider`,
+    );
   }
+
   return contextValue;
 };
