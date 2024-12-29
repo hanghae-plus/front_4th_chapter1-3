8c8226c64d2d3303960c5cde9b93151bc996063a
jungeun0712 <kje970712@naver.com>
2024-12-29 15:18:59 +0900

    fix: if문 수정

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,12 +1,14 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  // 두 값이 정확히 같은지 확인 (참조가 같은 경우)
+  // 기본 타입인 경우
   if (objA === objB) {
     return true;
   }
+
   // 둘 중 하나라도 객체가 아닌 경우 처리
   if (typeof objA !== "object" || typeof objB !== "object") {
     return false;
   }
+
   // 객체의 키 개수가 다른 경우 처리
   if (
     objA == null ||
@@ -15,8 +17,9 @@ export function shallowEquals<T>(objA: T, objB: T): boolean {
   ) {
     return false;
   }
+
   // 객체 안에 얕은 비교
-  if (objA && objB && !Array.isArray(objA) && !Array.isArray(objB)) {
+  if (typeof objA === "object" && typeof objB === "object") {
     for (const key in objA) {
       if (objA[key] !== objB[key]) {
         return false;
@@ -24,9 +27,11 @@ export function shallowEquals<T>(objA: T, objB: T): boolean {
     }
     return true;
   }
+
   // 배열 안에 얕은 비교
   if (Array.isArray(objA) && Array.isArray(objB)) {
     return objA.every((value, index) => value === objB[index]);
   }
+
   return false;
 }
