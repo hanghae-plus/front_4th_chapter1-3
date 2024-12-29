1e488564dab61a120bbb4c433cb97986479a4a49
nogy21 <nogy21@gmail.com>
2024-12-29 19:00:07 +0900

    feat: deepEquals 함수 깊은 비교 로직 구현
    - 두 값이 정확히 같은 경우 true
    - null, undefined인 경우 false (두 값이 같지 않기 때문)
    - 원시 타입인 경우 false (두 값이 같지 않기 때문)
    - 배열 비교
      - 길이 다를 경우 false
      - 배열 순회하며 deepEquals 함수를 재귀적으로 호출하여 배열 요소 비교
    - 객체 비교
      - 속성 개수 다를 경우 false
      - deepEquals 함수를 재귀적으로 호출하여 속성 비교
    - 그 외 true

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,45 @@
+/**
+ * 두 값의 깊은 비교를 수행하는 함수
+ * @param objA
+ * @param objB
+ * @returns {boolean}
+ */
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+  if (objA == null || objB == null) {
+    return false;
+  }
+  if (typeof objA !== "object" || typeof objB !== "object") {
+    return false;
+  }
+
+  // 배열 비교
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    for (let i = 0; i < objA.length; i++) {
+      if (!deepEquals(objA[i], objB[i])) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // 객체 비교
+  const keysA = Object.keys(objA) as Array<keyof T>;
+  const keysB = Object.keys(objB) as Array<keyof T>;
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+  for (let i = 0; i < keysA.length; i++) {
+    const key = keysA[i];
+    if (!deepEquals(objA[key], objB[key])) {
+      return false;
+    }
+  }
+
+  return true;
 }
