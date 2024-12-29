72aecff54368a9b38e6850f0d11fa8ece1514281
9yurilee <9yurilee@gmail.com>
2024-12-29 10:35:36 +0900

    feat: hooks 만들기

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,17 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { DependencyList } from "react";
+import { DependencyList, useRef } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef as myRef } from "./useRef";
 
 // 🎯 계산 비용이 높은 값을 메모이제이션.
-// 🎯 직접 작성한 useRef를 통해서 만들어보세요! 이게 제일 중요합니다.
-export function useMemo<T>(
-  factory: () => T,
-  _deps: DependencyList,
-  _equals = shallowEquals,
-): T {
+export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
   // 1. 이전 의존성과 결과를 저장할 ref 생성
+  const prevDeps = myRef<DependencyList | undefined>();
+  const prevResult = myRef<T | undefined>();
+
   // 2. 현재 의존성과 이전 의존성 비교
-  // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
+  if (!_deps || !prevDeps.current || !_equals(prevDeps.current, _deps)) {
+    // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
+    prevDeps.current = _deps;
+    prevResult.current = factory();
+  }
+
   // 4. 메모이제이션된 값 반환
-  return factory();
+  return prevResult.current as T;
 }
--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,5 +1,8 @@
+import { useState } from "react";
 // 🎯 렌더링 사이에 값을 유지하는 가변 ref 객체를 생성.
-// 🎯 React의 useState를 이용해서 만들어보세요. useRef를 구현하지 않으면 다른 hook을 구현할 수 없습니다.
-export function useRef<T>(initialValue: T): { current: T } {
-  return { current: initialValue };
+// 🎯 렌더링 간에 값이 유지되고, 값이 변경되어도 리렌더링을 트리거하지 않는다
+export function useRef<T>(initialValue?: T): { current: T } {
+  const [ref] = useState({ current: initialValue });
+
+  return ref as { current: T };
 }
