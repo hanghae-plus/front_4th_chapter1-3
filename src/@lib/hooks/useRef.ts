b719f0743104b11a0f8c84b202b97efeb4c4948b
Dayglow0926 <dayglow0926@gmail.com>
2024-12-29 23:55:08 +0900

    feat: useRef 기능 추가

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
   // React의 useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  const [ref] = useState<{ current: T }>({ current: initialValue });
+  /**
+   * ref.current를 통해 값이 변경이 가능한 이유
+   * ref(참조값)은 그대로 두고 .current (속성)을 변경하는 것이라 가능
+   */
+  return ref;
 }
