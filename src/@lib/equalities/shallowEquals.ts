3b39e2493623cf0249e6e6205756d0140a622dbd
김재환 <fodore12@gmail.com>
2024-12-29 23:05:30 +0900

    shallowEquals 함수 작성(수정 필요)

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,30 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  // 1. 기본 타입 값들을 정확히 비교해야 한다
+  if (objA === objB) return true;
+
+  // 2. 둘 중 하나라도 객체가 아닌 경우 처리
+  if (
+    typeof objA !== "object" ||
+    typeof objB !== "object" ||
+    objA === null ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  // 3. 객체의 키 개수가 다른 경우 처리
+  if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false;
+  }
+
+  // 4. 모든 키에 대해 얕은 비교 수행
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return Object.keys(objA).every((v, i) => objA[i] === objB[i]);
+  }
+
+  if (!Array.isArray(objA) && !Array.isArray(objB)) {
+    return Object.keys(objA).every((v) => objA[v] === objB[v]);
+  }
+
+  return false;
 }
