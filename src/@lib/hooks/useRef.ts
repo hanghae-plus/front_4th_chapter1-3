9e26d60238b068c3360d4d72f3cc4e8cbf0c2336
Gyeongmin Kim <gmkim716@gmail.com>
2024-12-29 19:22:29 +0900

    feat: useRef를 추가합니다

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 export function useRef<T>(initialValue: T): { current: T } {
-  // React의 useState를 이용해서 만들어보세요.
-  return { current: initialValue };
+  // useState 반환값의 첫번째만 사용
+  // useRef는 값이 변경되어도 리렌더링을 trigger하지 않아야 하므로 setState는 사용하지 않는다
+  // useRef의 표준 반환 타입과 일치시키기 위해 {current: T}를 추가한다
+  const [ref] = useState({ current: initialValue });
+
+  return ref;
 }
