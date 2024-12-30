b3a2b2d192f43aee2cc449a2cae024376224facf
jch1223 <ico1828@gmail.com>
2024-12-31 00:36:13 +0900

    useRef 함수 오버로드 추가

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,6 +1,13 @@
+import { MutableRefObject, RefObject } from "react";
 import React from "react";
 
-export function useRef<T>(initialValue?: T): { current?: T } {
-  const [ref] = React.useState<{ current?: T }>({ current: initialValue });
+export function useRef<T>(initialValue: T): MutableRefObject<T>;
+export function useRef<T>(initialValue: T | null): RefObject<T>;
+export function useRef<T = undefined>(): MutableRefObject<T | undefined>;
+
+export function useRef<T>(initialValue?: T) {
+  const [ref] = React.useState<{ current?: T }>({
+    current: initialValue,
+  });
   return ref;
 }
