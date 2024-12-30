d1cf1bd4c4bd4f50099919ef905c70fb0de6bc37
jungeun0712 <kje970712@naver.com>
2024-12-30 21:42:19 +0900

    feat: "HoC memo 구현"

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,22 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { shallowEquals } from "../equalities";
 import { ComponentType } from "react";
+import { shallowEquals } from "../equalities";
+import { useRef } from "../hooks";
+import React from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  // 메모이제이션된 컴포넌트
+  const MemoizedComponent = (props: P) => {
+    // 이전 props를 저장할 ref 생성
+    const prevProps = useRef<P | null>(null);
+    if (prevProps.current === null || !_equals(prevProps.current, props)) {
+      prevProps.current = props;
+      return React.createElement(Component, prevProps.current);
+    }
+  };
+
+  return MemoizedComponent;
 }
