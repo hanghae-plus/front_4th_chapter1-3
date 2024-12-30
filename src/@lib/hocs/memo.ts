f0486d390a0a07d6f9fcec8a03c9622ef068461d
jch1223 <ico1828@gmail.com>
2024-12-31 03:34:11 +0900

    memo 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement, ReactElement } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  return (props: P) => {
+    const memoizedProps = useRef<P>(props);
+    const memoizedComponent = useRef<ReactElement<P>>();
+
+    if (!memoizedComponent.current) {
+      memoizedComponent.current = createElement<P>(Component, props);
+    }
+
+    if (!_equals(memoizedProps.current, props)) {
+      memoizedProps.current = props;
+      memoizedComponent.current = createElement<P>(Component, props);
+    }
+
+    return memoizedComponent.current;
+  };
 }
