c7c088e475e224639ea173acc1deaafbde0d8007
JaeHyun Bang <jhbang@plea.kr>
2024-12-30 15:48:45 +0900

    feat : 얕은 복사(shallowEquals) 함수 작성

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,21 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) return true;
+  if (
+    typeof objA !== "object" ||
+    typeof objB !== "object" ||
+    objA === null ||
+    objB === null
+  )
+    return false;
+  const keyB = Object.keys(objB) as (keyof T)[];
+  const keyA = Object.keys(objA) as (keyof T)[];
+  if (keyB.length !== keyA.length) return false;
+  for (const key of keyA) {
+    if (
+      !Object.prototype.hasOwnProperty.call(objB, key) ||
+      objA[key] !== objB[key]
+    )
+      return false;
+  }
+  return true;
 }
