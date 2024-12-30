463bf4130ba8547773c2ea3b5587ab5110861b64
KimYoungWoo <rladuddn3924@gmail.com>
2024-12-30 19:37:21 +0900

    FEAT: equals 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,38 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
+  if (typeof objA !== typeof objB) {
+    return false;
+  }
+
+  if (
+    objA === null ||
+    objB === null ||
+    objA === undefined ||
+    objB === undefined
+  ) {
+    return objA === objB;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return (
+      objA.length === objB.length &&
+      objA.every((value, index) => deepEquals(value, objB[index]))
+    );
+  }
+
+  if (typeof objA === "object" && typeof objB === "object") {
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    return Object.keys(objA).every((key) => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-expect-error
+      return deepEquals(objA[key], objB[key]);
+    });
+  }
+
   return objA === objB;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,36 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  if (typeof objA !== typeof objB) {
+    return false;
+  }
+
+  if (
+    objA === null ||
+    objB === null ||
+    objA === undefined ||
+    objB === undefined
+  ) {
+    return objA === objB;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    return (
+      objA.length === objB.length &&
+      objA.every((value, index) => value === objB[index])
+    );
+  }
+
+  if (typeof objA === "object" && typeof objB === "object") {
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-expect-error
+    return Object.keys(objA).every((key) => objA[key] === objB[key]);
+  }
+
   return objA === objB;
 }
