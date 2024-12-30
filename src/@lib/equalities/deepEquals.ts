eca580c331e63e01ac1a61f8c8672be238551a3e
hyunjin <guswns1419@naver.com>
2024-12-31 04:04:25 +0900

    feat: equals 함수 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,36 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (
+    objA === null ||
+    objB === null ||
+    typeof objA !== "object" ||
+    typeof objB !== "object"
+  ) {
+    return false;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((itemA, index) => deepEquals(itemA, objB[index]));
+  }
+
+  if (!Array.isArray(objA) && !Array.isArray(objB)) {
+    const keysA = Object.keys(objA);
+    const keysB = Object.keys(objB);
+
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+
+    return keysA.every((key) =>
+      deepEquals(objA[key as keyof T], objB[key as keyof T]),
+    );
+  }
+
+  return false;
 }
