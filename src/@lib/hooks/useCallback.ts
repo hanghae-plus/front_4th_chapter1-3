cd930e7f4ce09491fd81395e586df0a8a251f757
Gyeongmin Kim <gmkim716@gmail.com>
2024-12-30 20:55:04 +0900

    feat: useCallback을 추가한다;

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,13 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
+import { useMemo } from "./useMemo";
+import { shallowEquals } from "../equalities";
 
 export function useCallback<T extends Function>(
-  factory: T,
-  _deps: DependencyList,
-) {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  callback: T,
+  deps: DependencyList,
+  equals = shallowEquals
+): T {
+  // useMemo를 사용하여 의존성이 변경될 때만 새로운 함수를 생성
+  return useMemo(() => callback, deps, equals);
 }
