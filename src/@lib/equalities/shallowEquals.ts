13ab1042779e3086d6fe24825af3377663dad0b6
장원정 <tazoal@tazoal.local>
2024-12-30 18:50:19 +0900

    feat: shallowEquals 함수 작성

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,32 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (Object.is(objA, objB)) {
+    return true;
+  }
+
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  const objAKeys = Object.keys(objA);
+  const objBKeys = Object.keys(objB);
+
+  if (objAKeys.length !== objBKeys.length) {
+    return false;
+  }
+
+  for (const key of objAKeys) {
+    if (
+      !Reflect.has(objB, key) ||
+      !Object.is(Reflect.get(objA, key), Reflect.get(objB, key))
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
