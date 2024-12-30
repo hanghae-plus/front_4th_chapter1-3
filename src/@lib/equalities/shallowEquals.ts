7dc126c7ee134a782a1b0689057829efb3c0b7f4
nopinokio <nopinokio90@gmail.com>
2024-12-30 21:04:32 +0900

    feat: shallowEquals 함수추가

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,27 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (objA === null || objB === null) {
+    return objA === objB;
+  }
+
+  if (typeof objA !== "object" || typeof objB !== "object") {
+    return objA === objB;
+  }
+
+  if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false;
+  }
+
+  for (const key of Object.keys(objA)) {
+    const keyA = Reflect.get(objA, key);
+    const keyB = Reflect.get(objB, key);
+    if (keyA !== keyB) {
+      return false;
+    }
+  }
+
+  return true;
 }
