3393edaf13bb806590d71fe292c2ec75c58759a7
wnsdn <vmfodzl1125@naver.com>
2024-12-30 23:12:01 +0900

    feat: useMemo를 사용하여 useDeepMemo 와 useCallback 구현 및 deepEquals 함수 개선

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,27 +1,33 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
   // 두 값이 정확히 같은 경우 true 반환
-  if (objA === objB) {
-    return true;
-  }
+  if (objA === objB) return true;
 
   // 1. 기본 타입이거나 null인 경우 처리
-  if (typeof objA !== 'object' || objA === null || objB === null || objB === undefined) return false
+  if (objA === null || objB === null || typeof objA !== 'object' || typeof objB !== 'object') {
+    return false;
+  }
 
-  // 2. 둘 다 객체인 경우:
-  if (typeof objA === 'object' && typeof objB === 'object') {
-    // 배열인지 확인
-    if (Array.isArray(objA) && Array.isArray(objB)) {
-      return objA.every((v, k) => deepEquals(v, objB[k]));
-    }
+  // 2. 배열 비교
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    // 배열 길이가 다르면 바로 false
+    if (objA.length !== objB.length) return false;
 
-    const objAKeys = Object.keys(objA)
-    const objBKeys = Object.keys(objB)
+    // 배열의 각 요소를 재귀적으로 비교
+    return objA.every((v, k) => deepEquals(v, objB[k]));
+  }
 
-    // 객체의 키 개수가 다른 경우 처리
+  // 3. 객체 비교
+  if (!Array.isArray(objA) && !Array.isArray(objB)) {
+    const objAKeys = Object.keys(objA);
+    const objBKeys = Object.keys(objB);
+
+    // 객체의 키 개수가 다르면 바로 false
     if (objAKeys.length !== objBKeys.length) return false;
 
     // 재귀적으로 각 속성에 대해 deepEquals 호출
-    return objAKeys.every(key => deepEquals((objA as Record<string, any>)[key], (objB as Record<string, any>)[key]));
+    return objAKeys.every(key =>
+      deepEquals((objA as Record<string, any>)[key], (objB as Record<string, any>)[key])
+    );
   }
-  return false
+  return false;
 }
--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -8,21 +8,21 @@ export function useMemo<T>(
   _deps: DependencyList, // 의존성 배열
   _equals = shallowEquals, // 얕은 비교 연산 함수
 ): T {
-  // 1. 이전 의존성과 결과를 저장할 ref 생성
-  const ref = useRef<{ deps: DependencyList; value: T | null}>({
-    deps: _deps,
-    value: null
-  });
+  // 1. 이전 의존성을 저장할 ref 생성
+  const prevDeps = useRef<DependencyList | null>(null); // 이전 의존성 저장
+
+  // 1. 계산된 값의 결과 저장할 ref 생성
+  const value = useRef<T | null>(null); 
 
   // 2. 현재 의존성과 이전 의존성 비교
-  const isDepsChanged = !_equals(ref.current.deps, _deps);
+  const isDepsChanged = !prevDeps.current || !_equals(prevDeps.current, _deps);
 
   if (isDepsChanged) {
     // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
-    ref.current.deps = _deps;
-    ref.current.value = factory();
+    prevDeps.current = _deps;  // 이전 의존성 업데이트
+    value.current = factory(); // 새로 계산된 값 저장
   }
 
   // 4. 메모이제이션된 값 반환
-  return ref.current.value!;
+  return value.current !== null ? value.current : factory();
 }
