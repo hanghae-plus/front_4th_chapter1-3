51704e2db1bf007cf6f606f77a9c90ee25b1c521
JaeHyun Bang <criticbanggu@gmail.com>
2024-12-30 17:15:21 +0900

    feat : 깊은 비교(deepEquals) 함수 작성

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,34 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) return true;
+  if (
+    typeof objA !== "object" ||
+    typeof objB !== "object" ||
+    objA === null ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((item, index) => deepEquals(item, objB[index]));
+  }
+
+  if (Array.isArray(objA) || Array.isArray(objB)) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA) as (keyof T)[];
+  const keysB = Object.keys(objB) as (keyof T)[];
+
+  if (keysA.length !== keysB.length) return false;
+  for (const key of keysA) {
+    if (!keysB.includes(key) || !deepEquals(objA[key], objB[key])) {
+      return false;
+    }
+  }
+
+  return true;
 }
