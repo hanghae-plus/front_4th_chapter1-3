a3887a92ab93d04e6a7d061193697ae9d17b7769
장원정 <tazoal@tazoal.local>
2024-12-31 19:21:38 +0900

    feat: React.memo 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,24 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import React, { ComponentType } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  const MemoizedComponent = (props: P) => {
+    const memoizedProps = useRef<P | null>(null);
+
+    if (memoizedProps.current === null) {
+      memoizedProps.current = props;
+      return React.createElement(Component, props);
+    }
+
+    if (!_equals(memoizedProps.current, props)) {
+      memoizedProps.current = props;
+      return React.createElement(Component, props);
+    }
+  };
+
+  return MemoizedComponent;
 }
