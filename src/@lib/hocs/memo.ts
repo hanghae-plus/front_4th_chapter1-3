fa8366dae385e5289de881395938a50f6ca3cd06
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-12-31 22:25:13 +0900

    feat: hooks 기능 구현
    - useRef, useMemo, useCallback, memo 기능 구현
    - basic test 통과

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement, ReactElement } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
-  _equals = shallowEquals,
-) {
-  return Component;
+  equals = shallowEquals,
+): ComponentType<P> {
+  return (props: P) => {
+    const ref = useRef<{ component: ReactElement; props: P } | null>(null);
+
+    if (ref.current === null) {
+      ref.current = {
+        component: createElement(Component, props),
+        props,
+      };
+
+      return ref.current.component;
+    }
+
+    if (!equals(ref.current.props, props)) {
+      ref.current = {
+        component: createElement(Component, props),
+        props,
+      };
+    }
+
+    return ref.current.component;
+  };
 }
