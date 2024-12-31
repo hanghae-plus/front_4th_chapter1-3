c178103d0e955c1f9a5cbc9c677409ff1e23f211
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-12-31 15:34:46 +0900

    feat: shallowEquals, deepEquals 함수 추가
    - 얕은 비교, 깊은 비교 함수 추가

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,22 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (typeof objA !== "object" || typeof objB !== "object") {
+    return false;
+  }
+
+  if (objA === null || objB === null) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA) as (keyof T)[];
+  const keysB = Object.keys(objB) as (keyof T)[];
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  return keysA.every((key) => objA[key] === objB[key]);
 }
