08702128584b7866c5c3900eb68ff4b809be7df3
confidential-nt <youyoy689@gmail.com>
2025-01-01 00:55:37 +0900

    feat: shallowEquals 구현

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,36 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (Object.is(objA, objB)) {
+    return true;
+  }
+
+  if (typeof objA !== "object" || typeof objB !== "object") {
+    return false;
+  }
+
+  if (objA === null || objB === null) {
+    return false;
+  }
+
+  // 객체인 경우 참조가 다르더라도 키-값이 같으면 true (주의: 1 depth만)
+  const objAKeys = Object.keys(objA);
+  const objBKeys = Object.keys(objB);
+
+  if (objAKeys.length !== objBKeys.length) {
+    return false;
+  }
+
+  for (let i = 0; i < objAKeys.length; i++) {
+    const key = objAKeys[i];
+    if (
+      !Object.prototype.hasOwnProperty.call(objB, key) ||
+      !Object.is(
+        (objA as Record<string, unknown>)[key],
+        (objB as Record<string, unknown>)[key],
+      )
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
