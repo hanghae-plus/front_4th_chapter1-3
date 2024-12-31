36e3537cef59db76af682e241ee0b9b66ae7f75e
jungeun0712 <kje970712@naver.com>
2024-12-31 23:09:44 +0900

    feat: 테마 변경 추가

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,13 @@ const ThemeProvider: React.FC<PropsWithChildren> = ({ children }) => {
   const value = useMemo(() => ({ theme, toggleTheme }), [theme, toggleTheme]);
 
   return (
-    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+    <ThemeContext.Provider value={value}>
+      <div
+        className={`min-h-screen ${theme === "light" ? "bg-gray-100" : "bg-gray-900 text-white"}`}
+      >
+        {children}
+      </div>
+    </ThemeContext.Provider>
   );
 };
 
