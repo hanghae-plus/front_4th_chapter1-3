a466c248f37bb389cf9e992b9324ba4d68db7cc6
YeongseoYoon <l112xpla@gmail.com>
2024-12-31 14:19:32 +0900

    feat: memo 함수 구현

--- src/@lib/hocs/memo.tsx
+++ src/@lib/hocs/memo.tsx
@@ -1,4 +1,4 @@
-import { ComponentType, createElement } from "react";
+import { ComponentType, ReactNode } from "react";
 
 import { shallowEquals } from "../equalities";
 import { useRef } from "../hooks";
@@ -20,14 +20,16 @@ export function memo<P extends object>(
   // 2. 메모이제이션된 컴포넌트 생성
   const MemoizedComponent = function (props: P) {
     const prevPropsRef = useRef(props);
+    const memoizedComponent = useRef<ReactNode | null>(null);
 
     // 3. equals 함수를 사용하여 props 비교
     // 4. props가 변경된 경우에만 새로운 렌더링 수행
-    if (!_equals(prevPropsRef.current, props)) {
+    if (!memoizedComponent.current || !_equals(prevPropsRef.current, props)) {
       prevPropsRef.current = props;
+      memoizedComponent.current = <Component {...props} />;
     }
 
-    return createElement(Component, prevPropsRef.current);
+    return memoizedComponent.current;
   };
 
   MemoizedComponent.displayName = `Memo(${Component.displayName || Component.name || "Component"})`;
