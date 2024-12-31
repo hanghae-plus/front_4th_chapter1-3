f4feaa01abdde004ff9debcae4b510fc2eb44654
melroh629 <jw.roh629@gmail.com>
2024-12-31 10:07:13 +0900

    feat: shallowEquals 함수 추가

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,28 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
+  if (objA === objB) {
+    return true;
+  }
+  if (objA == null || objB == null) {
+    return false;
+  }
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((_, index) => objA[index] === objB[index]);
+  }
+  if (typeof objA === "object" && typeof objB === "object") {
+    const keysA = Object.keys(objA as object);
+    const keysB = Object.keys(objB as object);
+    if (keysA.length !== keysB.length) {
+      return false;
+    }
+    return keysA.every(
+      (item) =>
+        Object.prototype.hasOwnProperty.call(objB, item) &&
+        (objA as { [key: string]: unknown })[item] ===
+          (objB as { [key: string]: unknown })[item],
+    );
+  }
   return objA === objB;
 }
