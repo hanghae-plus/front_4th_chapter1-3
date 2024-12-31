b3f12ea3775e0abccb3b4ffbd37d4883447ff5a3
confidential-nt <youyoy689@gmail.com>
2025-01-01 01:46:39 +0900

    feat: useCallback 구현

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,17 @@
-/* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
+import { useMemo } from "./useMemo";
+
+// useCallback의 역할: 리렌더링시에도 함수의 참조를 유지하는 것
+// useMemo를 활용하라네?
+// _deps가 바뀌면? 함수를 재생성
+// _deps가 안바뀌면? 기존의 함수 리턴
 
 export function useCallback<T extends Function>(
   factory: T,
   _deps: DependencyList,
 ) {
-  // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const factoryMemo = useMemo(() => factory, _deps);
+  return factoryMemo;
 }
