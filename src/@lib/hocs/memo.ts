c58eb2f81129ce7006c7d0671596ae11161845b0
DESKTOP-PPQ6A24\USER <thddl2079@naver.com>
2024-12-31 17:26:29 +0900

    기본과제 통과

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -15,7 +15,7 @@ export function memo<P extends object>(
   // 4. props가 변경된 경우에만 새로운 렌더링 수행
 
   const MemoizedComponent = (props: P): ReactElement => {
-    /* const propsRef = useRef<{ component: ReactElement; props: P } | null>(null);
+    const propsRef = useRef<{ component: ReactElement; props: P } | null>(null);
 
     if (!propsRef.current || !_equals(propsRef.current.props, props)) {
       propsRef.current = {
@@ -24,17 +24,7 @@ export function memo<P extends object>(
       };
     }
 
-    return propsRef.current.component; */
-
-    const prevPropsRef = useRef<P | null>(null);
-    const renderPropsRef = useRef<React.ReactElement | null>(null);
-
-    if (!prevPropsRef.current || !_equals(prevPropsRef.current, props)) {
-      prevPropsRef.current = props;
-      renderPropsRef.current = createElement(Component, props);
-    }
-
-    return createElement(Component, props);
+    return propsRef.current.component;
   };
 
   return MemoizedComponent;
@@ -45,7 +35,7 @@ export function memo<P extends object>(
     const prevPropsRef = useRef<P | null>(null);
     const renderPropsRef = useRef<React.ReactElement | null>(null);
 
-    if (!prevPropsRef.current || _equals(prevPropsRef.current, props)) {
+    if (!prevPropsRef.current || !_equals(prevPropsRef.current, props)) {
       prevPropsRef.current = props;
       renderPropsRef.current = React.createElement(Component, props);
     }
