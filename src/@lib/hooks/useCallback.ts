8b6c30546c64d002c1bbfab11022518124ef1830
Changyeop Song <waitwait0301@naver.com>
2025-01-01 21:20:38 +0900

    fix: useCallback의 내부로직을 useMemo로 대체

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,21 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
-import { useRef } from "./useRef";
-import { shallowEquals } from "../equalities";
+
+import { useMemo } from "./useMemo";
 
 export function useCallback<T extends Function>(
   factory: T,
   _deps: DependencyList,
 ) {
-  const ref = useRef<null | { deps: DependencyList; factory: T }>(null);
-
-  // deps에 변화가 생기면 함수을 리턴
-  if (!ref.current || !shallowEquals(_deps, ref.current.deps)) {
-    ref.current = {
-      deps: _deps,
-      factory,
-    };
-  }
-
-  return ref.current.factory;
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return useMemo(() => factory, _deps);
 }
