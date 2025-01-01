386f009e0f81a4308e5a87626be3bcff7d5bec8c
Changyeop Song <waitwait0301@naver.com>
2025-01-01 20:52:10 +0900

    refactor: memo 함수 내에서 반복되는 로직 개선

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -3,6 +3,10 @@ import { ComponentType, createElement, ReactElement } from "react";
 import { useRef } from "../hooks";
 
 // memo HOC는 컴포넌트의 props를 얕은 비교하여 불필요한 리렌더링을 방지합니다.
+// 1. 이전 props를 저장할 ref 생성
+// 2. 메모이제이션된 컴포넌트 생성
+// 3. equals 함수를 사용하여 props 비교
+// 4. props가 변경된 경우에만 새로운 렌더링 수행
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
@@ -14,37 +18,22 @@ export function memo<P extends object>(
       props: P;
     }>(null);
 
-    // 초기 current가 비어있는 경우
-    if (ref.current === null) {
-      ref.current = {
+    const createNewComponent = () => {
+      const newComponent = {
         component: createElement(Component, props),
         props,
       };
-
-      return ref.current.component;
-    }
-
-    // props가 같을 경우
-    if (_equals(ref.current?.props, props)) {
-      return ref.current?.component;
-    }
-
-    // props가 같지 않을 경우
-    ref.current = {
-      component: createElement(Component, props),
-      props,
+      ref.current = newComponent;
+      return newComponent.component;
     };
+
+    // 초기 렌더링이거나 props가 변경된 경우 새로운 컴포넌트 생성
+    if (ref.current === null || !_equals(ref.current.props, props)) {
+      return createNewComponent();
+    }
 
     return ref.current.component;
   };
 
   return MemoizedComponent;
 }
-
-// 1. 이전 props를 저장할 ref 생성
-
-// 2. 메모이제이션된 컴포넌트 생성
-
-// 3. equals 함수를 사용하여 props 비교
-
-// 4. props가 변경된 경우에만 새로운 렌더링 수행
