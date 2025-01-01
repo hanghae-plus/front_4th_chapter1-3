b29bc94d291c50a2877715823d23671f3f4d173a
Blue-Kite <bluelemon112@gmail.com>
2025-01-01 11:52:30 +0900

    feat: shallowEquals, deepEquals 함수 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,48 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (
+    objA === null ||
+    objB === null ||
+    typeof objA !== "object" ||
+    typeof objB !== "object"
+  ) {
+    return objA === objB;
+  }
+
+  //둘다 배열인 경우
+  if (Array.isArray(objA) && Array.isArray(objB)) {
+    if (objA.length !== objB.length) {
+      return false;
+    }
+    return objA.every((item, index) => deepEquals(item, objB[index]));
+  }
+
+  //하나만 배열
+  if (Array.isArray(objA) || Array.isArray(objB)) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (const k of keysA) {
+    if (
+      !(k in objB) ||
+      !deepEquals(
+        (objA as Record<string, unknown>)[k],
+        (objB as Record<string, unknown>)[k],
+      )
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,32 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+  if (keysA.length !== keysB.length) {
+    return false;
+  }
+
+  for (const k of keysA) {
+    if (
+      !(k in objB) ||
+      (objA as Record<string, unknown>)[k] !==
+        (objB as Record<string, unknown>)[k]
+    ) {
+      return false;
+    }
+  }
+
+  return true;
 }
