72c8a367756306043235db0db5c22fab97d2b2cc
nopinokio <nopinokio90@gmail.com>
2025-01-01 22:33:37 +0900

    feat: memo, useRef, useMemo, useCallback 추가

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useRef } from "./useRef";
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
 
@@ -7,6 +7,17 @@ export function useMemo<T>(
   _deps: DependencyList,
   _equals = shallowEquals,
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  // 메모이제이션된 값과 의존성을 저장
+  const cachedValue = useRef<T>();
+  const cachedDeps = useRef<DependencyList>();
+
+  // 이전 의존성과 현재 의존성을 비교
+  if (!cachedDeps.current || !_equals(cachedDeps.current, _deps)) {
+    // 의존성이 변경된 경우, 새로운 값을 계산하고 저장
+    cachedDeps.current = _deps;
+    cachedValue.current = factory();
+  }
+
+  // 메모이제이션된 값 반환
+  return cachedValue.current!;
 }
