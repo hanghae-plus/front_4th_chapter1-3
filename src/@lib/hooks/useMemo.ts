8eeb823e4095e401628a5f4191032b7bd26c8a58
Blue-Kite <bluelemon112@gmail.com>
2025-01-01 13:33:22 +0900

    feat: useCallback, useMemo 구현

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,12 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(
   factory: () => T,
   _deps: DependencyList,
   _equals = shallowEquals,
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  const ref = useRef<{
+    deps: DependencyList | null;
+    result: T | null;
+  }>({
+    deps: null,
+    result: null,
+  });
+
+  // 2. 현재 의존성과 이전 의존성 비교
+  const shouldUpdate =
+    ref.current.deps === null || !_equals(ref.current.deps, _deps);
+
+  if (shouldUpdate) {
+    ref.current.deps = _deps;
+    ref.current.result = factory();
+  }
+
+  return ref.current.result!;
 }
--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,6 +1,5 @@
 import { useState } from "react";
 export function useRef<T>(initialValue: T): { current: T } {
-  // React의 useState를 이용해서 만들어보세요.
   const [ref] = useState<{ current: T }>({ current: initialValue });
   return ref;
 }
