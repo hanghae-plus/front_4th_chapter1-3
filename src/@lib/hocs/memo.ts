1507af657a77985764a973a617bc295618ca6258
devsuzy <susie0529@naver.com>
2025-01-02 00:44:39 +0900

    feat: 기본 과제 통과

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,14 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { shallowEquals } from '../equalities';
+import { ComponentType, createElement, ReactElement, useRef } from 'react';
 
-export function memo<P extends object>(
-  Component: ComponentType<P>,
-  _equals = shallowEquals,
-) {
+export function memo<P extends object>(Component: ComponentType<P>, _equals = shallowEquals) {
   // 1. 이전 props를 저장할 ref 생성
   // 2. 메모이제이션된 컴포넌트 생성
   // 3. equals 함수를 사용하여 props 비교
   // 4. props가 변경된 경우에만 새로운 렌더링 수행
-  return Component;
+
+  const MemoizedComponent = (props: P) => {
+    const prevPropsRef = useRef<{ component: ReactElement; props: P } | null>(null);
+
+    if (!prevPropsRef.current || !_equals(prevPropsRef.current.props, props)) {
+      prevPropsRef.current = {
+        component: createElement(Component, props),
+        props,
+      };
+    }
+
+    return prevPropsRef.current.component;
+  };
+
+  return MemoizedComponent;
 }
--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,26 +1,21 @@
-import { DependencyList } from "react";
-import { shallowEquals } from "../equalities";
-import { useRef } from "./useRef";
+import { DependencyList } from 'react';
+import { shallowEquals } from '../equalities';
+import { useRef } from './useRef';
 
-export function useMemo<T>(
-  factory: () => T,
-  _deps: DependencyList,
-  _equals = shallowEquals,
-): T {
+export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
   // 1. 이전 의존성과 결과를 저장할 ref 생성
   // 2. 현재 의존성과 이전 의존성 비교
   // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
   // 4. 메모이제이션된 값 반환
 
-  const ref = useRef<{ value: T; deps: DependencyList | undefined }>({
-    value: undefined as T,
-    deps: undefined,
-  });
+  const ref = useRef<null | { value: T; _deps: DependencyList | undefined }>(null);
 
-  if (!ref.current || !_equals(_deps, ref.current.deps)) {
-    ref.current.value = factory();
-    ref.current.deps = _deps;
+  if (!ref.current || !_equals(_deps, ref.current._deps)) {
+    ref.current = {
+      _deps,
+      value: factory(),
+    };
   }
 
-  return ref.current?.value;
+  return ref.current.value;
 }
