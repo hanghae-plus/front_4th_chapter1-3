329a7589a52bf26845c2a0022e4b82c1996c4658
KimBoYoung <qhdud20@naver.com>
2025-01-01 15:51:24 +0900

    feat: 비교 함수 완료

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,22 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) return true;
+
+  if (objA === null || objB === null || typeof objA !== "object" || typeof objB !== "object") return false;
+  
+  const isArrayA = Array.isArray(objA);
+  const isArrayB = Array.isArray(objB);
+  if (isArrayA && isArrayB) {
+    if (objA.length !== objB.length) return false;
+    return objA.every((item, index) => deepEquals(item, objB[index]));
+  }
+
+  const keysA = Object.keys(objA);
+  const keysB = Object.keys(objB);
+  if (keysA.length !== keysB.length) return false;
+
+  for (const key of keysA) 
+    if (!deepEquals(objA[key as keyof T], objB[key as keyof T]))
+      return false
+  
+  return true;
 }
