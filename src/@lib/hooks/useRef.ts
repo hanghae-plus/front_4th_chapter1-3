c24aa75a49e4e7a533821bf80977c2955ceb0518
chb6734 <chb6734@gmail.com>
2025-01-02 02:04:45 +0900

    feat : useMemo test 통과

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -2,6 +2,6 @@ import { useState } from "react";
 
 export function useRef<T>(initialValue: T): { current: T } {
   // React의 useState를 이용해서 만들어보세요.
-  const [ref] = useState({ current: initialValue });
+  const [ref] = useState<{ current: T }>({ current: initialValue });
   return ref;
 }
