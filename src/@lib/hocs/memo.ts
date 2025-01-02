f454111c03746510f49450e0f8b945b2aeed9425
melroh629 <jw.roh629@gmail.com>
2025-01-02 12:20:08 +0900

    feat: memo함수 추가

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,27 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { ComponentType, createElement } from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  const MemoizedComponent = (props: P) => {
+    return createElement(Component, props);
+  };
+
+  MemoizedComponent.displayName = `Memo(${Component.displayName || Component.name})`;
+
+  let prevProps: P | undefined;
+  let prevElement: React.ReactElement | null = null;
+
+  return Object.assign(
+    (props: P) => {
+      if (!prevProps || !_equals(prevProps, props)) {
+        prevElement = createElement(MemoizedComponent, props);
+        prevProps = props;
+      }
+      return prevElement;
+    },
+    { displayName: MemoizedComponent.displayName },
+  );
 }
