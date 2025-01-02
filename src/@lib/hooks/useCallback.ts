ea8a1627f14731d02fb67736b1baade4ba6e81d7
LESANF <nagong1000@naver.com>
2025-01-02 20:24:33 +0900

    feat: useCallback 구현

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,10 +1,9 @@
 /* eslint-disable @typescript-eslint/no-unused-vars,@typescript-eslint/no-unsafe-function-type */
 import { DependencyList } from "react";
+import { useMemo } from "./useMemo";
 
-export function useCallback<T extends Function>(
-  factory: T,
-  _deps: DependencyList,
-) {
+export function useCallback<T extends Function>(factory: T, _deps: DependencyList) {
+  const memoizedFactory = useMemo(() => factory, _deps);
   // 직접 작성한 useMemo를 통해서 만들어보세요.
-  return factory as T;
+  return memoizedFactory;
 }
