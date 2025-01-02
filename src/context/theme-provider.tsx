0907d6ab5592e004dc7f2f75a77fd81123f05e96
CodyMan0 <hys83751952@gmail.com>
2025-01-02 22:58:47 +0900

    :sparkles: ThemeProvider 분리

---
+++ src/context/theme-provider.tsx
@@ -0,0 +1,38 @@
+import { createContext, PropsWithChildren, useContext, useState } from "react";
+
+interface ThemeContextType {
+  theme: string | undefined;
+}
+
+const ThemeContext = createContext<ThemeContextType["theme"]>(undefined);
+const ThemeUpdateContext = createContext((_themeContext: string) => {});
+export const ThemeProvider = ({ children }: PropsWithChildren) => {
+  const [theme, setTheme] = useState("light");
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return (
+    <ThemeContext.Provider value={theme}>
+      <ThemeUpdateContext.Provider value={toggleTheme}>
+        {children}
+      </ThemeUpdateContext.Provider>
+    </ThemeContext.Provider>
+  );
+};
+
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useThemeContext must be used within an ThemeProvider");
+  }
+  return context;
+};
+
+export const useUpdateThemeContext = () => {
+  const context = useContext(ThemeUpdateContext);
+  if (context === undefined) {
+    throw new Error("useThemeContext must be used within an ThemeProvider");
+  }
+  return context;
+};
