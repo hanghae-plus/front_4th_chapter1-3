249228ad4f65e80854aa53fe0b91c3626ad8c71b
Hyeyeon(Anne) <khy2106@naver.com>
2025-01-02 22:56:45 +0900

    fix: useMemo 타입 수정

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,31 +1,28 @@
-import { DependencyList } from "react";
-import { useRef } from "./useRef";
+import { FC, ReactNode, createElement } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "../hooks";
 
-type MemoState<T> = {
-  deps: DependencyList;
-  value: T;
-};
+export function memo<P extends object>(
+  Component: FC<P>,
+  equals = shallowEquals,
+): FC<P> {
+  const MemoizedComponent: FC<P> = (props) => {
+    // 이전 props를 저장할 ref 생성
+    const prevPropsRef = useRef<P | null>(null);
+    const renderedRef = useRef<ReactNode>(null);
 
-type CompareFn = (a: DependencyList, b: DependencyList) => boolean;
+    // props 비교
+    if (
+      prevPropsRef.current === null ||
+      !equals(prevPropsRef.current, props) ||
+      renderedRef.current === null
+    ) {
+      prevPropsRef.current = props;
+      renderedRef.current = createElement(Component, props);
+    }
 
-export function useMemo<T>(
-  factory: () => T,
-  deps: DependencyList,
-  equals: CompareFn = shallowEquals,
-): T {
-  // 1. 이전 상태를 저장할 ref 생성
-  const ref = useRef<MemoState<T> | null>(null);
+    return renderedRef.current as ReactNode;
+  };
 
-  // 2. 현재 의존성과 이전 의존성을 비교
-  if (!ref.current || !equals(deps, ref.current.deps)) {
-    // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
-    ref.current = {
-      deps,
-      value: factory(),
-    };
-  }
-
-  // 4. 메모이제이션된 값 반환
-  return ref.current.value;
+  return MemoizedComponent;
 }
