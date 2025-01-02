9c05c5f8f09ea5d4664d799d65e6427710a7fd5d
zinoopark <93peace@naver.com>
2025-01-02 15:05:15 +0900

    feat: deepEquals 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,14 @@
 export function deepEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) return true;
+  if (!objA || !objB || typeof objA !== "object" || typeof objB !== "object")
+    return false;
+
+  //obj 비교
+  const keysA = Object.keys(objA) as (keyof T)[];
+  const keysB = Object.keys(objB) as (keyof T)[];
+
+  return (
+    keysA.length === keysB.length &&
+    keysA.every((key) => key in objB && deepEquals(objA[key], objB[key]))
+  );
 }
