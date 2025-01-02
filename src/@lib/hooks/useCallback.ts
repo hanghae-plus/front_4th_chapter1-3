affa9e558cbe426322af84c825e6fa240073c498
Hyeyeon(Anne) <khy2106@naver.com>
2025-01-02 22:56:26 +0900

    fix: useCallback 타입 수정

--- src/@lib/hooks/useCallback.ts
+++ src/@lib/hooks/useCallback.ts
@@ -1,9 +1,6 @@
 import { DependencyList } from "react";
 import { useMemo } from "./useMemo";
 
-export function useCallback<T extends (...args: unknown[]) => unknown>(
-  factory: T,
-  deps: DependencyList,
-): T {
+export function useCallback<T>(factory: T, deps: DependencyList): T {
   return useMemo(() => factory, deps);
 }
