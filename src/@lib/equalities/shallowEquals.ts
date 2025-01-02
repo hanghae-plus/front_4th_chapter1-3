2812d83458528e0cdf0d4acf62ca72c1d9ed30ea
divopsor <divopsor@gmail.com>
2025-01-03 02:40:01 +0900

    feat: equalities 추가 및 assertObject 추가

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,30 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+
+  if (
+    typeof objA !== "object" ||
+    objA == null ||
+    typeof objB !== "object" ||
+    objB == null
+  ) {
+    return false;
+  }
+
+  if (Object.keys(objA).length !== Object.keys(objB).length) {
+    return false;
+  }
+
+  for (const key in objA) {
+    if (!Object.prototype.hasOwnProperty.call(objB, key)) {
+      return false;
+    }
+
+    if (objA[key] !== objB[key]) {
+      return false;
+    }
+  }
+
+  return true;
 }
