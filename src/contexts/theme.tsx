95c5e1f642397fffa1a4692ad615858c896cd8d7
divopsor <divopsor@gmail.com>
2025-01-03 03:09:03 +0900

    feat: appContext 를 3개로 분리

---
+++ src/contexts/theme.tsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState } from "react";
+
+// ThemeContext 타입 정의
+export interface ThemeContextType {
+  theme: string;
+  toggleTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useThemeContext = () => {
+  const context = useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useThemeContext must be used within an AppProvider");
+  }
+  return context;
+};
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  const themeContextValue: ThemeContextType = {
+    theme,
+    toggleTheme,
+  };
+
+  return (
+    <ThemeContext.Provider value={themeContextValue}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
