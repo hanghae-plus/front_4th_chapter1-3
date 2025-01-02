a88ff189b8f42fb9d727bd09f0805e0dca721592
Hyeyeon(Anne) <khy2106@naver.com>
2025-01-02 21:20:04 +0900

    feat: memo 함수 생성

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ComponentType, createElement, ReactNode } from "react";
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
-  _equals = shallowEquals,
-) {
-  return Component;
+  equals = shallowEquals,
+): ComponentType<P> {
+  function MemoizedComponent(props: P) {
+    // 이전 props를 저장할 ref 생성
+    const prevPropsRef = useRef<P | null>(null);
+    const renderedRef = useRef<ReactNode>(null);
+
+    // props 비교
+    if (
+      prevPropsRef.current === null ||
+      !equals(prevPropsRef.current, props) ||
+      renderedRef.current === null
+    ) {
+      prevPropsRef.current = props;
+      renderedRef.current = createElement(Component, props);
+    }
+
+    return renderedRef.current;
+  }
+
+  return MemoizedComponent;
 }
