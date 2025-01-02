08f782bf58d16db2dc59e5a2779750fbadf4c50d
JaeHyun Bang <criticbanggu@gmail.com>
2025-01-02 11:42:36 +0900

    feat : memo HOC 작성

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,19 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
 import { ComponentType } from "react";
+import { useRef } from "../hooks";
+import React from "react";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
-  _equals = shallowEquals,
+  _equals = shallowEquals
 ) {
-  return Component;
+  const memoComponent = (props: P) => {
+    const prevElement = useRef<P | null>(null);
+    if (prevElement.current === null || !_equals(prevElement.current, props)) {
+      prevElement.current = props;
+      return React.createElement(Component, prevElement.current);
+    }
+  };
+  return memoComponent;
 }
