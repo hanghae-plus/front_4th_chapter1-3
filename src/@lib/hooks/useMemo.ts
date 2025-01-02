ce82bf04eedd8e13c73120112ec511374ad6431e
devsuzy <susie0529@naver.com>
2025-01-02 23:13:46 +0900

    feat: context 구현

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,14 +1,20 @@
-import { DependencyList } from 'react';
-import { shallowEquals } from '../equalities';
-import { useRef } from './useRef';
+import { DependencyList } from "react";
+import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
-export function useMemo<T>(factory: () => T, _deps: DependencyList, _equals = shallowEquals): T {
+export function useMemo<T>(
+  factory: () => T,
+  _deps: DependencyList,
+  _equals = shallowEquals,
+): T {
   // 1. 이전 의존성과 결과를 저장할 ref 생성
   // 2. 현재 의존성과 이전 의존성 비교
   // 3. 의존성이 변경된 경우 factory 함수 실행 및 결과 저장
   // 4. 메모이제이션된 값 반환
 
-  const ref = useRef<null | { value: T; _deps: DependencyList | undefined }>(null);
+  const ref = useRef<null | { value: T; _deps: DependencyList | undefined }>(
+    null,
+  );
 
   if (!ref.current || !_equals(_deps, ref.current._deps)) {
     ref.current = {
