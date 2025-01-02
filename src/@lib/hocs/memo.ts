607bc6062da8a9bc8a5858ebf059e0be45e8d7c1
CodyMan0 <hys83751952@gmail.com>
2025-01-02 09:09:27 +0900

    :sparkles: memo 로직 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,18 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { ComponentType } from "react";
+import { ComponentType, createElement } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "../hooks";
 
-// memo HOC는 컴포넌트의 props를 얕은 비교하여 불필요한 리렌더링을 방지합니다.
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  // 1. 이전 props를 저장할 ref 생성
+  return (props: P) => {
+    const prevPropsRef = useRef<P | null>(null);
+    const renderRef = useRef<React.ReactElement | null>(null);
 
-  // 2. 메모이제이션된 컴포넌트 생성
-
-  // 3. equals 함수를 사용하여 props 비교
-
-  // 4. props가 변경된 경우에만 새로운 렌더링 수행
-  return Component;
+    if (!prevPropsRef.current || !_equals(prevPropsRef.current, props)) {
+      prevPropsRef.current = props;
+      renderRef.current = createElement(Component, props);
+    }
+    return renderRef.current;
+  };
 }
