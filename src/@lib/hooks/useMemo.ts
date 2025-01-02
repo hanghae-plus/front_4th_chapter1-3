86fb02e7a64498ccb2980ebc369e46747e09a3dc
WonYoung <wy.heo@teamsparta.co>
2025-01-02 20:29:40 +0900

    fix: ts error 수정

--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -2,14 +2,11 @@ import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
 import { useRef } from "./useRef";
 
-// Q: useMemo는 Object.is로 비교하는 것으로 알고 있는데 과제에서는 왜 직접구현한 shallowEquals를 사용해서 비교하는가?
 export function useMemo<T>(
   factory: () => T,
   _deps: DependencyList,
   _equals = shallowEquals,
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  // 이 ref가 다시 호출되어서 factory가 호출됨
   const value = useRef<T | null>(null);
   const deps = useRef(_deps);
 
