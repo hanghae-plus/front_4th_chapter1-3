b0b1e812985c65cf0d296b2d9a1db436cc93653a
Dayglow0926 <dayglow0926@gmail.com>
2025-01-03 00:06:17 +0900

    fix: 얕은 비교 기능 수정

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,7 +1,6 @@
-export function shallowEquals<T extends Record<string, unknown>>(
-  objA: T,
-  objB: T,
-): boolean {
+import { isRecord } from "../../utils";
+
+export function shallowEquals<T>(objA: T, objB: T): boolean {
   // 1. 두 값이 정확히 같은지 확인 (참조가 같은 경우)
   if (objA === objB) {
     return true;
@@ -9,27 +8,21 @@ export function shallowEquals<T extends Record<string, unknown>>(
 
   // 2. 둘 중 하나라도 객체가 아닌 경우 처리
   if (
-    typeof objA !== "object" ||
-    objA === null ||
-    typeof objB !== "object" ||
-    objB === null
+    Array.isArray(objA) &&
+    Array.isArray(objB) &&
+    objA.length === objB.length
   ) {
-    return false;
-  }
-
-  // 3. 키 개수 비교
-  const keys1 = Object.keys(objA);
-  const keys2 = Object.keys(objB);
-  if (keys1.length !== keys2.length) {
-    return false;
+    return objA.every((v, i) => v === objB[i]);
   }
 
   // 4. 모든 키에 대해 얕은 비교
-  for (const key of keys1) {
-    if (objA[key] !== objB[key]) {
+  if (isRecord(objA) && isRecord(objB)) {
+    // 3. 키 개수 비교
+    if (Object.keys(objA).length !== Object.keys(objB).length) {
       return false;
     }
+    return Object.keys(objA).every((key) => objA[key] === objB[key]);
   }
 
-  return true;
+  return false;
 }
