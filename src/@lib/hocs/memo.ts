07310bea2d29680f6f37650d95f35de4d62d08a3
YeongseoYoon <l112xpla@gmail.com>
2025-01-02 21:18:13 +0900

    refactor: memo 로직 개선

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,7 +1,6 @@
-import { ComponentType, ReactNode } from "react";
+import { ComponentType, createElement } from "react";
 
 import { shallowEquals } from "../equalities";
-import { useRef } from "../hooks";
 
 /**
  * 컴포넌트의 props를 비교하여 불필요한 리렌더링을 방지하는 HOC
@@ -18,21 +17,19 @@ export function memo<P extends object>(
 ): ComponentType<P> {
   // 1. 이전 props를 저장할 ref 생성
   // 2. 메모이제이션된 컴포넌트 생성
-  const MemoizedComponent = function (props: P) {
-    const prevPropsRef = useRef(props);
-    const memoizedComponent = useRef<ReactNode | null>(null);
+  let prevProps: P | null = null;
+  let memoizedElement: ReturnType<typeof createElement> | null = null;
 
+  const MemoizedComponent = function (props: P) {
     // 3. equals 함수를 사용하여 props 비교
     // 4. props가 변경된 경우에만 새로운 렌더링 수행
-    if (!memoizedComponent.current || !_equals(prevPropsRef.current, props)) {
-      prevPropsRef.current = props;
-      memoizedComponent.current = <Component {...props} />;
+    if (!prevProps || !_equals(prevProps, props)) {
+      prevProps = props;
+      memoizedElement = createElement(Component, props);
     }
 
-    return memoizedComponent.current;
+    return memoizedElement;
   };
-
-  MemoizedComponent.displayName = `Memo(${Component.displayName || Component.name || "Component"})`;
 
   return MemoizedComponent;
 }
