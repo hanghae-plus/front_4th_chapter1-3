4a6bd4dc36930680cd328682946c9a6c8171fb8c
hanmok <dmammmm@naver.com>
2025-01-03 00:18:35 +0900

    add comment

--- src/@lib/hooks/useRef.ts
+++ src/@lib/hooks/useRef.ts
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+// 변경되지 않는 값을 저장하거나 컴포넌트 리렌더링 간에 값을 유지하는 용도로 사용
 export function useRef<T>(initialValue: T): { current: T } {
   // ref: 상태값, 컴포넌트가 리렌더링 될때마다 새로운 값으로 갱신될 수 있음.
   // 그러나 useState 는 객체를 리렌더링 시점에 고정할 수 있는 방식으로 상태를 유지.
