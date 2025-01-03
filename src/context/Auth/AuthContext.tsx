8418775dc0c2454dc1ca81788567f8630a58b8ac
lshyun955 <lshyun955@gmail.com>
2025-01-03 11:50:20 +0900

    수정: 유저 상태 및 핸들러 context 관리로 분리

---
+++ src/context/Auth/AuthContext.tsx
@@ -0,0 +1,39 @@
+import { createContext, useState } from "react";
+import { User } from "../../types";
+import { useCallback, useMemo } from "../../@lib";
+import { useAppContext } from "../App/useAppContext";
+import { AuthContextType } from "./types";
+
+export const AuthContext = createContext<AuthContextType | undefined>(
+  undefined,
+);
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const { addNotification } = useAppContext();
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = useCallback(
+    (email: string) => {
+      setUser({ id: 1, email, name: "홍길동" });
+      addNotification("성공적으로 로그인되었습니다", "success");
+    },
+    [addNotification],
+  );
+
+  const logout = useCallback(() => {
+    setUser(null);
+    addNotification("로그아웃되었습니다", "info");
+  }, [addNotification]);
+
+  const authInfo: AuthContextType = useMemo(() => {
+    return {
+      user,
+      login,
+      logout,
+    };
+  }, [user, login, logout]);
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
